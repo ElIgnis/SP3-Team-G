a806c88285b9187848568919c84fff681f82e2e6
JohnLeong <johnleongjs@gmail.com>
2015-08-17 17:43:24 +0800

    Added basic enemy class

--- Base/Source/Enemy.cpp
+++ Base/Source/Enemy.cpp
@@ -1,9 +1,28 @@
 #include "Enemy.h"
 
 CEnemy::CEnemy() 
+	: m_bPatrolDir(true)
+	, m_iCurrentPatrolpoint(0)
 {
 }
 
 CEnemy::~CEnemy()
 {
+}
+
+void CEnemy::Update(const double dt)
+{
+	switch(state)
+	{
+	case STATE_PATROL:
+		pos += (m_patrolposList[m_iCurrentPatrolpoint + 1] - pos).Normalize() * dt * 10;
+		break;
+	case STATE_WAIT:
+		m_fWaitTime -= dt;
+		if(m_fWaitTime < 0)
+			state = STATE_PATROL;
+		break;
+	default:
+		break;
+	}
 }
--- Base/Source/Enemy.h
+++ Base/Source/Enemy.h
@@ -14,14 +14,23 @@ public:
 	CEnemy();
 	~CEnemy();
 
+	void Update(const double dt);
+
 	enum ENEMY_STATE
 	{
 		STATE_PATROL,
+		STATE_WAIT,
 		STATE_ATTACK,
 		NUM_STATES,
 	};
 
-	std::vector<Vector3 *> m_patrolposList;
+	std::vector<Vector3> m_patrolposList;
+
+private:
+	int m_iCurrentPatrolpoint;
+	bool m_bPatrolDir;
+	float m_fWaitTime;
+	ENEMY_STATE state;
 };
 
 #endif
