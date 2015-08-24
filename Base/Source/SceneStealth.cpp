c626fc93e58e81b361535a68f4fdd948230456f2
JoelIsidore <ruby_crownclown@yahoo.com.sg>
2015-08-24 09:24:48 +0800

    Started on Game Level Design

--- Base/Source/SceneStealth.cpp
+++ Base/Source/SceneStealth.cpp
@@ -67,7 +67,7 @@ void SceneStealth::InitGame(void)
 
 	//Initializing the player
 	Virus = new CPlayer;
-	Virus->pos.Set(1,0,0);
+	Virus->pos.Set(-95,40,0);
 	Virus->scale.Set(10,10,10);
 	Virus->mass = 1.f;
 }
