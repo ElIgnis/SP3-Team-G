96b71fcc254eedd35f243434261a705bfcee9b3d
JohnLeong <johnleongjs@gmail.com>
2015-08-31 23:11:59 +0800

    Changed font

--- Base/Source/SceneBase.cpp
+++ Base/Source/SceneBase.cpp
@@ -174,7 +174,7 @@ void SceneBase::Init()
 	}
 
 	//Text
-	meshList[GEO_TEXT]->textureID = LoadTGA("Image//PixelMix.tga");
+	meshList[GEO_TEXT]->textureID = LoadTGA("Image//RetroComputer.tga");
 	meshList[GEO_TEXT]->material.kAmbient.Set(1, 0, 0);
 
 	//Game Objects
