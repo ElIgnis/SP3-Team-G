45437c5e880b2e15fcc935cbe72a1cf5aec8a91a
JeremiahOriendo <toxicantcoffee@gmail.com>
2015-09-02 11:42:45 +0800

    Volume things, again.

--- Base/Source/GGAudio.cpp
+++ Base/Source/GGAudio.cpp
@@ -27,27 +27,27 @@ int SceneStealth::InitAudio()
 	sound[LEVEL_BUTTON]->setVolume(0.7f);
 
 	sound[LEVEL_CHECKPOINT] = engine->play2D("../Base/Audio/Level_checkpoint.mp3", false, true);
-	sound[LEVEL_CHECKPOINT]->setVolume(0.5f);
+	sound[LEVEL_CHECKPOINT]->setVolume(1.0f);
 
 	//Enemy sounds here
 	sound[ENEMY_ALERT] = engine->play2D("../Base/Audio/Enemy_alert.mp3", false, true);
-	sound[ENEMY_ALERT]->setVolume(1.0f);
+	sound[ENEMY_ALERT]->setVolume(0.5f);
 	
 	sound[ENEMY_SHOOT] = engine->play2D("../Base/Audio/Enemy_shoot.ogg", false, true);
-	sound[ENEMY_SHOOT]->setVolume(1.0f);
+	sound[ENEMY_SHOOT]->setVolume(4.0f);
 	
 	sound[ENEMY_BULLET_WALL] = engine->play2D("../Base/Audio/Enemy_shoot_hit_wall.wav", false, true);
-	sound[ENEMY_BULLET_WALL]->setVolume(0.08f);
+	sound[ENEMY_BULLET_WALL]->setVolume(0.05f);
 	
 	sound[ENEMY_STUNNED] = engine->play2D("../Base/Audio/Enemy_stunned.wav", false, true);
 	sound[ENEMY_STUNNED]->setVolume(0.5f);
 
 	//Player Sounds here
 	sound[PLAYER_DMG] = engine->play2D("../Base/Audio/Player_damaged.wav", false, true);
-	sound[PLAYER_DMG]->setVolume(0.8f);
+	sound[PLAYER_DMG]->setVolume(0.3f);
 	
 	sound[PLAYER_PICKUP] = engine->play2D("../Base/Audio/Player_pickup.wav", false, true);
-	sound[PLAYER_PICKUP]->setVolume(0.8f);
+	sound[PLAYER_PICKUP]->setVolume(1.0f);
 	
 	sound[PLAYER_HEALTH] = engine->play2D("../Base/Audio/Player_health.wav", false, true);
 	sound[PLAYER_HEALTH]->setVolume(0.9f);
@@ -59,7 +59,7 @@ int SceneStealth::InitAudio()
 	sound[PLAYER_SPEED]->setVolume(0.9f);
 	
 	sound[PLAYER_DECOY] = engine->play2D("../Base/Audio/Player_decoy.wav", false, true);
-	sound[PLAYER_DECOY]->setVolume(0.9f);
+	sound[PLAYER_DECOY]->setVolume(0.07f);
 
 	return 0;
 }
